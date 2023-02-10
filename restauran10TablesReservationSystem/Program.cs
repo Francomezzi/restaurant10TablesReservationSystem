fbed411fd91e51883fc31e42ac726e812fe871c3
Franco Mezzi <mezzifrancodev@gmail.com>
2023-02-10 16:09:58 -0300

    Creamos las primeras variables y Arrays

--- restauran10TablesReservationSystem/Program.cs
+++ restauran10TablesReservationSystem/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+/*
+ * 1.Validar si el usuario existe o si se debe registar.Generar un sistema de registro o login.
+ * 2.El programa debe ser capaz de darle la bienvenida si el usuario exite.
+ * 3.El programa debe repetirse hasta que se registren 10 personas.
+ */
+
+string[] userNames = new string[10]{ "Miguel","","","","","","","","",""};
+int currentIndex = 1;
