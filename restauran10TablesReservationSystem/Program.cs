f20c4ad0584f8e732ad229a6143e107c5f3753ce
Franco Mezzi <mezzifrancodev@gmail.com>
2023-02-10 16:39:15 -0300

    Terminamos el programa de manera conforme, pero quedan optimizaciones por realizar

--- restauran10TablesReservationSystem/Program.cs
+++ restauran10TablesReservationSystem/Program.cs
@@ -6,19 +6,41 @@
  * 3.El programa debe repetirse hasta que se registren 10 personas.
  */
 
-string[] userNames = new string[10]{ "Miguel","","","","","","","","",""};
-int currentIndex = 1;
+using System.Security;
+
+string[] userNames = new string[10]{ "","","","","","","","","",""};
+int currentIndex = 0;
 bool userType;
 
 Console.WriteLine("Welcome to the best restaurant in the world");
 while (true)
 {
-    Console.WriteLine("Are you a registered user? Write true, or false to register.");
+    if (currentIndex == 10)
+    {
+        Console.WriteLine("The restaurant is full, try again next year.");
+        Environment.Exit(0);
+    }
+    Console.WriteLine("\n Are you a registered user? Write true, or false to register.");
     userType = Convert.ToBoolean(Console.ReadLine());
     if (userType == true)
     {
-        Console.WriteLine("Hello, you are a registered user, please enter your exact user name");
+        Console.WriteLine("\n Hello, you are a registered user, please enter your exact user name.");
         string userToSearch = Console.ReadLine();
-        Console.WriteLine("The user you searched is {0}",userToSearch);
+        int index = Array.IndexOf(userNames, userToSearch);
+        if (index == -1)
+        {
+            Console.WriteLine("User not found, try again or register.");
+        }
+        else
+        {
+            Console.WriteLine("Welcome {0}, it is a pleasure to serve you.", userNames[index]);
+        }
+    } 
+    else if (userType == false)
+    {
+        Console.WriteLine("Please write and remember your user name");
+        userNames[currentIndex] = Console.ReadLine();
+        Console.WriteLine("Your user has been saved succesfully\n" + "Your user name is {0}", userNames[currentIndex]);
+        currentIndex++;
     }
 }
